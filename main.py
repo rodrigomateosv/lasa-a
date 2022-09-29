08202c089b52333901e5cd805e26580f267ac3f5
Rodrigo Mateos <114070557+rodrigomateosv@users.noreply.github.com>
2022-09-29 11:28:15 +0000

    cambio4

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+EXPECTED_BAKE_TIME = 40
+PREPARATION_TIME = 2
 
 def bake_time_remaining(elapsed_bake_time):
     """Calculate the bake time remaining.
@@ -10,7 +12,7 @@ def bake_time_remaining(elapsed_bake_time):
    based on the `EXPECTED_BAKE_TIME`.
    """
     return EXPECTED_BAKE_TIME - elapsed_bake_time
-  def preparation_time_in_minutes(number_of_layers):
+def preparation_time_in_minutes(number_of_layers):
     """Calculate the preparation time.
  
     :param number_of_layers: int the number of lasagna layers made
